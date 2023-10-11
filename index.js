ee981e17e92f2083b690f3c6ca2b8fcb0864f9c8
Aditya Singh <aditya@cleverstudio.in>
2023-10-11 23:06:58 +0530

    Added Cors support 3

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 app.use(
 	cors({
 		origin: [
-			'http://127.0.0.1:5173',
+			'http://localhost:5173',
 			'http://127.0.0.1:5174',
 			'https://www.firdausmedia.com',
 		],
